e948599eb00372179b6070557d257b0bb15a0159
Jaeyoon Choi <jae.yoon.cc.gmail.com>
2022-03-03 17:45:58 +0900

    feat: addEmployee 함수 구현
    - 추가된 employee 개수를 얻어올 수 있는 getEmployeeCount()
      함수 추가

--- EmployeeManager/EmployeeManagement.cpp
+++ EmployeeManager/EmployeeManagement.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <utility>
+#include <stdexcept>
 #include "EmployeeManagement.h"
 
 using namespace std;
@@ -8,8 +9,17 @@ void EmployeeManagement::processCmd(int command, vector<int> options, vector<Inf
 
 }
 
-bool EmployeeManagement::addEmployee(Employee& employee) {
-	return true;
+void EmployeeManagement::addEmployee(Employee& employee) {
+	if (employee.employee_num == "") {
+		throw invalid_argument("invalid employee information");
+		return;
+	}
+
+	auto ret = employee_map_.insert(make_pair(employee.employee_num, employee));
+
+	if (ret.second == false) {
+		throw invalid_argument("fail to add employee");
+	}
 }
 
 unique_ptr<vector<Employee>> EmployeeManagement::deleteEmployee(const Inform info) {
@@ -25,4 +35,8 @@ unique_ptr<vector<Employee>> EmployeeManagement::searchEmployee(const Inform inf
 unique_ptr<vector<Employee>> EmployeeManagement::modifyEmployee(const Inform search_info, const Inform modify_info) {
 	auto employees = make_unique<vector<Employee>>();
 	return move(employees);
+}
+
+const size_t EmployeeManagement::getEmployeeCount() const {
+	return employee_map_.size();
 }
