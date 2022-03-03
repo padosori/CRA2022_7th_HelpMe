70a3829f3b64ab579b84e641b3d596900a4009b3
hansoo <snk1587@gmail.com>
2022-03-03 13:37:12 +0900

    feat: Employee, Imform 구조체 생성

---
+++ EmployeeManager/Employee.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+using namespace std;
+
+struct inform {
+	string column;
+	string value;
+};
+
+struct Employee {
+	string employee_num;
+	string name;
+	string first_name;
+	string last_name;
+
+	string phone_num;
+	string mid_phone_num;
+	string last_phone_num;
+
+	string birthday;
+	string birthday_year;
+	string birthday_month;
+	string birthday_day;
+
+	string cl;
+	string certi;
+};
