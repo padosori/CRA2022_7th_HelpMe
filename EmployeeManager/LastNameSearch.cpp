03c651e0e145b535474ea5a30d5cc963f5d922f4
hansoo <snk1587@gmail.com>
2022-03-04 14:44:07 +0900

    feat: First & Last name search 구현

---
+++ EmployeeManager/LastNameSearch.cpp
@@ -0,0 +1,12 @@
+#include "LastNameSearch.h"
+
+unique_ptr<vector<Employee>> LastNameSearch::search(map<std::string, Employee>& employee_map, Inform condition)
+{
+	auto result = std::make_unique<vector<Employee>>();
+	result->clear();
+	if (condition.column != "name_last") return move(result);
+	for (auto employee : employee_map) {
+		if (employee.second.last_name == condition.value) result->emplace_back(employee.second);
+	}
+	return move(result);
+}
---
+++ Test_EmployeeManager/TestFirstNameSearch.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "TestSearch.h"
+#include "../EmployeeManager/FirstNameSearch.cpp"
+
+using TestFirstNameSearch = TestSearch;
+
+TEST_F(TestFirstNameSearch, TestSearchOneItem) {
+	auto searcher = make_unique<FirstNameSearch>();
+	Inform inform = { "name_first", "Gildong" };
+	auto result = searcher->search(*employeeMap, inform);
+	EXPECT_EQ(1, result->size());
+	EXPECT_EQ("Gildong Hong", (*result)[0].name);
+}
+
+TEST_F(TestFirstNameSearch, TestSearchManyItem) {
+	auto searcher = make_unique<FirstNameSearch>();
+	Inform inform = { "name_first", "Gyoungrae" };
+	auto result = searcher->search(*employeeMap, inform);
+	EXPECT_EQ(2, result->size());
+	EXPECT_EQ("01001122", (*result)[0].employee_num);
+	EXPECT_EQ("99112233", (*result)[1].employee_num);
+}
+
+TEST_F(TestFirstNameSearch, TestWrongName) {
+	auto searcher = make_unique<FirstNameSearch>();
+	Inform inform = { "name_first", "ENDMA" };
+	auto result = searcher->search(*employeeMap, inform);
+	EXPECT_EQ(0, result->size());
+}
+
+TEST_F(TestFirstNameSearch, TestWrongColumn) {
+	auto searcher = make_unique<FirstNameSearch>();
+	Inform inform = { "cl", "2015123099" };
+	auto result = searcher->search(*employeeMap, inform);
+	EXPECT_EQ(0, result->size());
+}
