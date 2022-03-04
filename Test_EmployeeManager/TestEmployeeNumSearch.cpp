cdf628988a4feed432c58ad1a232405bd3fd889c
hansoo <snk1587@gmail.com>
2022-03-04 09:20:05 +0900

    style: 한 라인 charater 제한 수정

--- Test_EmployeeManager/TestEmployeeNumSearch.cpp
+++ Test_EmployeeManager/TestEmployeeNumSearch.cpp
@@ -7,9 +7,18 @@ class TestEmployeeNumSearch : public ::testing::Test {
 protected:
 	void SetUp() override {
 		employee_num_search_ = std::make_shared<EmployeeNumSearch>();
-		employee_map_.insert({ "2015123099", { "2015123099", "VXIHXOTH JHOP", "VXIHXOTH", "JHOP", "010 - 3112 - 2609", "3112", "2609", "19771211", "1977", "12", "11", "CL3", "ADV" } });
-		employee_map_.insert({ "2017112609", { "2017112609","FB NTAWR","FB","NTAWR", "010 - 5645 - 6122","5645", "6122", "19861203","1986","12","03", "CL4", "PRO" } });
-		employee_map_.insert({ "1988114052", { "1988114052","TTETHU HBO","TTETHU","HBO", "010 - 4581 - 2050","4581", "2050", "20080718","2008","07","18", "CL2", "ADV" } });
+		employee_map_.insert(
+			{ "2015123099", 
+			{ "2015123099", "VXIHXOTH JHOP", "VXIHXOTH", "JHOP", "010 - 3112 - 2609", "3112", "2609", 
+			  "19771211", "1977", "12", "11", "CL3", "ADV" } });
+		employee_map_.insert(
+			{ "2017112609", 
+			{ "2017112609","FB NTAWR","FB","NTAWR", "010 - 5645 - 6122","5645", "6122", 
+			  "19861203","1986","12","03", "CL4", "PRO" } });
+		employee_map_.insert(
+			{ "1988114052", 
+			{ "1988114052","TTETHU HBO","TTETHU","HBO", "010 - 4581 - 2050","4581", "2050", 
+			  "20080718","2008","07","18", "CL2", "ADV" } });
 	}
 
 	void TearDown() override {
