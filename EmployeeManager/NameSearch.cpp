069e2ce30986455fa1a654a5b404feb143f6818c
hansoo <snk1587@gmail.com>
2022-03-04 10:42:05 +0900

    feat: NameSearch 구현

---
+++ EmployeeManager/NameSearch.cpp
@@ -0,0 +1,11 @@
+#include "NameSearch.h"
+
+unique_ptr<vector<Employee>> NameSearch::search(map<std::string, Employee>& employee_map, Inform condition) {
+	auto result = std::make_unique<vector<Employee>>();
+	result->clear();
+	if (condition.column != "name") return move(result);
+	for (auto employee : employee_map) {
+		if (employee.second.name == condition.value) result->emplace_back(employee.second);
+	}
+	return move(result);
+}
