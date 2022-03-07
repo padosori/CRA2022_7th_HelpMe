d28cb7d192843d46b22569ebb69fee009e894a9f
hansoo <snk1587@gmail.com>
2022-03-07 10:38:23 +0900

    feat: search factory 구현

---
+++ EmployeeManager/SearchFactory.cpp
@@ -0,0 +1,21 @@
+#include "SearchFactory.h"
+#include <stdexcept>
+std::unique_ptr<Search> SearchFactory::getSearch(std::string column)
+{
+	if (column == "name") { return std::make_unique<NameSearch>(); }
+	else if (column == "name_first") { return std::make_unique<FirstNameSearch>(); }
+	else if (column == "name_last") { return std::make_unique<LastNameSearch>(); }
+	else if (column == "phoneNum") { return std::make_unique<PhoneNumSearch>(); }
+	else if (column == "phoneNum_middle") { return std::make_unique<MiddlePhoneNumSearch>(); }
+	else if (column == "phoneNum_last") { return std::make_unique<LastPhoneNumSearch>(); }
+	else if (column == "birthday") { return std::make_unique<BirthdaySearch>(); }
+	else if (column == "birthday_year") { return std::make_unique<BirthdayYearSearch>(); }
+	else if (column == "birthday_month") { return std::make_unique<BirthdayMonthSearch>(); }
+	else if (column == "birthday_day") { return std::make_unique<BirthdayDaySearch>(); }
+	else if (column == "employeeNum") { return std::make_unique<EmployeeNumSearch>(); }
+	else if (column == "certi") { return std::make_unique<CertiSearch>(); }
+	else if (column == "cl") { return std::make_unique<ClSearch>(); }
+	else { 
+		throw invalid_argument("invalid column");
+	}
+}
